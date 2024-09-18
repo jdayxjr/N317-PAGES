9018a11c172b1b1e7e8d15da7c43700595e2022e
jdayxjr <49356758+jdayxjr@users.noreply.github.com>
2024-09-18 11:35:15 -0400

    now can search pokemon with images

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,41 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [score, setScore] = useState(1);
+
+  //Deconstructing arrays: 
+  //1.the variable names use for the items don't matter
+  2.
+
+  //let score=0;
+
+  function addToScore() {
+    //score++
+
+    // setScore(score + 1)
+
+    //you can set a new value by passing a new value is the setter function
+    // or by passing a new function to the setter function
+    setScore(function (value) {
+      return value + 1;
+    })
+    console.log(score)
+
+    function doublePoints() {
+      addToScore();
+      addToScore();
+    }
+  }
+
   return (
     <main>
       <h1>Home</h1>
+      <div>
+        <button onClick={addToScore}>Add 1</button>
+        <p>Score: {score}</p>
+      </div>
     </main>
   );
 }
